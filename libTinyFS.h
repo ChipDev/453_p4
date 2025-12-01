b6937e1a76922f3db8e4a58246f48a353b205cf0
Dwight Anthony Taylor Jr <dtaylo37@unix5.csc.calpoly.edu>
2025-12-01 13:45:04 -0800

    added error header file and function definitions for phase 2.

--- libTinyFS.h
+++ libTinyFS.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /* The default size of the disk and file system block */
 #define BLOCKSIZE 256
 
@@ -10,3 +14,23 @@
 
 /* Use as a special type to keep track of files */
 typedef int fileDescriptor;
+
+/* Function definitions */
+
+int tfs_mkfs(char *filename, int nBytes);
+
+int tfs_mount(char *diskname);
+
+int tfs_unmount(void);
+
+fileDescriptor tfs_openFile(char *name);
+
+int tfs_closeFile(fileDescriptor FD);
+
+int tfs_writeFile(fileDescriptor FD, char *buffer, int size);
+
+int tfs_deleteFile(fileDescriptor FD);
+
+int tfs_readByte(fileDescriptor FD, char *buffer);
+
+int tfs_seek(fileDescriptor FD, int offset);
