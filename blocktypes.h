680e805b69ff1daa4dfc8923e70a16c9bcb3755f
Blake R. Stewart <bstewa11@unix5.csc.calpoly.edu>
2025-12-01 13:50:15 -0800

    superblock struct

--- blocktypes.h
+++ blocktypes.h
@@ -4,9 +4,23 @@
  * to make it modular and easy to digest and debug.
 */
 
+#include <stdint.h>
+
+#define MAGIC 0x44
+#define SB_E (256 - 1 - 1 - 4 - 4) 
+
 typedef enum {
 	SUPERBLOCK = 1,
 	INODE = 2,
 	FILEEXTENT = 3,
 	FREE = 4
 } blocktype;
+
+typedef struct {
+	uint8_t blocktype;	// byte	0	: SUPERBLOCK (1)
+	uint8_t magic;		// byte 1	: MAGIC (0x44)
+	int32_t root_inode;	// byte 2:5	: root inode of fs
+	int32_t free_list;	// byte 6:9	: block # of free list
+	uint8_t empty[SB_E];	// byte 10:255 : reserved
+} superblock_disk;
+
