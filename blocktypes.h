2f49486030cec4131e8f76edb2d6ae5e7df250b4
Blake Stewart <bstewa11@cts-techrentals-131299.tail0526de.ts.net>
2025-12-02 11:09:40 -0800

    blocktypes finalized*

--- blocktypes.h
+++ blocktypes.h
@@ -8,7 +8,9 @@
 
 #define MAGIC 0x44
 #define SB_E (256 - 1 - 1 - 4 - 4) 
-
+#define IN_E (256 - 1 - 1 - 9 - 4 -4 - 1)
+#define EX_E (256 - 1 - 1 - 4)
+#define FR_E (256 - 1 - 1 - 4)
 typedef enum {
 	SUPERBLOCK = 1,
 	INODE = 2,
@@ -24,3 +26,26 @@ typedef struct {
 	uint8_t empty[SB_E];	// byte 10:255 : reserved
 } superblock_disk;
 
+typedef struct {
+	uint8_t blocktype;	//byte 0	: INODE (2)
+	uint8_t magic;		//byte 1	: MAGIC (0x44)
+	uint8_t name[9];	//byte 2-10	: NAME [8 chars + guaranteed \0] 
+	int32_t size_B;		//byte 11-14	: SIZE bytes
+	int32_t blk_start;	//byte 15-18	: BLOCK of initial data (can be -1)
+	uint8_t metaflags;	//byte 19	: FLAGS (extra metdata tbd)
+	uint8_t empty[IN_E];
+} inode_disk; 
+
+typedef struct {
+	uint8_t blocktype;	//byte 0 	: FILEEXTENT (3)
+	uint8_t magic;		//byte 1	: MAGIC 0x44
+	int32_t blk_next; 	//byte 2-5	: BLOCK of next data (can be -1)
+	uint8_t data[EX_E];	//byte 6-255	: DATA 
+} fileextent_disk;
+
+typedef struct {
+	uint8_t blocktype; 	//byte 0	: FREE (4)
+	uint8_t magic;		//byte 1 	: MAGIC
+	int32_t blk_next; 	//byte 2-5 	: BLOCK of next free data (can be -1)
+	uint8_t empty[FR_E];	//byte 6-255 	: EMPT
+} free_disk;	
